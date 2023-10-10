c45e6e57c5a6762c1d1e95e66c9a1760d147ee0f
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-10-10 18:30:04 -0300

    clase 1: inicio

--- Semana01/clase1.js
+++ Semana01/clase1.js
@@ -3,24 +3,11 @@
 /* -------------------------------------------------------------------------- */
 function iniciarJuego() {
 
-    // suludamos al usuario
-    alert("Bienvenido al piedra papel o tijera de Frontend II.");
-    // guardamos en una variable en nombre ingresado
-    const nombre = prompt("Ingese su nombre por favor:")
 
-    alert("Gracias por jugar " + nombre + ". ¡Mucha suerte!");
-
-    // mostramos los datos por consola
-    console.log("----------------------------");
-    console.log("El jugador es:")
-    console.log(nombre);
-    console.log("----------------------------");
-
-    return nombre;
 }
 
 // creamos una variable a nivel global para guardar el nombre del jugador que nos devuelve la función
-const nombreJugador = iniciarJuego();
+
 
 /* -------------------------------------------------------------------------- */
 /*                          CONSIGNA MESA DE TRABAJO                          */
