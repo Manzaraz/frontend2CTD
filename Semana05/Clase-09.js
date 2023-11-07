c80553903102421f5d09765942b4ce4d4fecd9f8
Christian Manzaraz <magnusmanz@gmail.com>
2023-11-07 18:15:55 -0300

    Clase 9: inicio de clase

---
+++ Semana05/Clase-09.js
@@ -0,0 +1,101 @@
+/* -------------------------- estado por defecto ------------------------- */
+const estadoUsuario = {
+    email: "",
+    password: "",
+    rol: "",
+    terminos: false
+};
+
+// ponemos en true solo cuando estén correctos
+const estadoErroresOK = {
+    email: false,
+    password: false,
+    rol: false,
+    terminos: false
+};
+
+/* ---------------------------------- nodos --------------------------------- */
+
+// capturamos todos los elementos que necesitamos
+const formulario = document.forms[0];
+
+const inputEmail = document.querySelector('#email');
+const inputPassword = document.querySelector('#password');
+const inputRol = document.querySelector('#rol');
+const inputTerminos = document.querySelector('#terminos');
+
+const emailError = document.querySelector('#emailError');
+const passwordError = document.querySelector('#passwordError');
+const rolError = document.querySelector('#rolError');
+const terminosError = document.querySelector('#terminosError');
+
+
+/* -------------------------------------------------------------------------- */
+/*                   [1] FUNCION: mostrar errores al usuario                  */
+/* -------------------------------------------------------------------------- */
+function mostrarErrores() {
+    
+
+}
+
+/* -------------------------------------------------------------------------- */
+/*               [2] FUNCION: actulizamos los estados de la app               */
+/* -------------------------------------------------------------------------- */
+
+// 👇 por cada cambio en el formulario actualizamos
+formulario.addEventListener('change', function () {
+
+    
+});
+
+
+/* -------------------------------------------------------------------------- */
+/*                        [3] FUNCIONES: validar campos                       */
+/* -------------------------------------------------------------------------- */
+function validarEmail(email) {
+    
+}
+
+function validarPassword(password) {
+    
+}
+
+function validarRol(rol) {
+   
+
+}
+
+function validarTerminos(verificacion) {
+    
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                      [4] FUNCION: escuchamos el submit                     */
+/* -------------------------------------------------------------------------- */
+
+// en el evento submit nos remitimos a chequear nuestro estado de errores
+formulario.addEventListener('submit', function (evento) {
+   
+});
+
+
+
+
+/* ----------------------------- MESA DE TRABAJO ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                [5] FUNCION: Formulario completado con éxito                */
+/* -------------------------------------------------------------------------- */
+// Esta funcion se va a encargar de realizar la redirección cuando el formulario se complete correctamente.
+// Para eso debera cumplir con los siguientes requerimientos.
+// 1 - Deshabilitar el boton del formulario.
+// 2 - Esperar 3 segundos para redireccionar a la página de usuario.html
+// 3 - Durante ese tiempo el boton deshabilitado debe mostrar el texto: "Cargando..."
+// 4 - Cuando vaya a la página de 'usuario.html' NO se debe permitir que mediante el botón de "Atrás"(la flechita del navegador) el usuario vuelva a index.
+// evento location.
+// location.replace()
+
+function navegarPaginaExito() {
+    // desarrollar la funcion aqui 
+    
+}
